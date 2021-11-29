3e5f70017049270cee20b35ce285b717dea7cb7d
Gabriela Coy <42784440+GabiMath@users.noreply.github.com>
2021-11-29 04:00:40 -0500

    Add files via upload

---
+++ CalculadoraCambioBase.js
@@ -0,0 +1,93 @@
+class CalculadoraCambioBase {
+  constructor() {
+    this.pila = new Array();
+    this.display = "";
+  }
+
+  digito(numero) {
+    this.display += numero.toString();
+    document.getElementById("display").innerHTML = this.display;
+  }
+
+  cambioBase2() {
+    var resultado = Number(this.display).toString(2);
+    document.getElementById("actual").innerHTML = resultado;
+  }
+
+  cambioBase3() {
+    var resultado = Number(this.display).toString(3);
+    document.getElementById("actual").innerHTML = resultado;
+  }
+
+  cambioBase5() {
+    var resultado = Number(this.display).toString(5);
+    document.getElementById("actual").innerHTML = resultado;
+  }
+
+  cambioBase7() {
+    var resultado = Number(this.display).toString(7);
+    document.getElementById("actual").innerHTML = resultado;
+  }
+
+  cambioBase8() {
+    var resultado = Number(this.display).toString(8);
+    document.getElementById("actual").innerHTML = resultado;
+  }
+
+  cambioBase10() {
+    var resultado = Number(this.display).toString(10);
+    document.getElementById("actual").innerHTML = resultado;
+  }
+
+  cambioBase12() {
+    var resultado = Number(this.display).toString(12);
+    document.getElementById("actual").innerHTML = resultado;
+  }
+
+  cambioBase16() {
+    var resultado = Number(this.display).toString(16);
+    document.getElementById("actual").innerHTML = resultado;
+  }
+
+  cambioBase20() {
+    var resultado = Number(this.display).toString(20);
+    document.getElementById("actual").innerHTML = resultado;
+  }
+
+  borrar() {
+    var nuevo = this.display.substring(0, this.display.length - 1);
+    this.display = nuevo;
+    document.getElementById("display").innerHTML = this.display;
+  }
+
+  borrarTodo() {
+    this.display = "";
+    document.getElementById("display").innerHTML = this.display;
+    document.getElementById("actual").innerHTML = "";
+  }
+
+  teclado(evento) {
+    const numeros = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    if (numeros.includes(evento.key)) {
+      this.digito(evento.key);
+    } else if (evento.key == "Backspace") {
+      this.borrar();
+    } else if (evento.key == "Delete") {
+      this.borrarTodo();
+    } else if (evento.key == "b") {
+      this.cambioBase2();
+    } else if (evento.key == "o") {
+      this.cambioBase8();
+    } else if (evento.key == "d") {
+      this.cambioBase10();
+    } else if (evento.key == "h") {
+      this.cambioBase16();
+    }
+  }
+}
+
+var calculador = new CalculadoraCambioBase();
+
+document.addEventListener("keydown", (evento) => {
+  calculador.teclado(evento.key);
+});
