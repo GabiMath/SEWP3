36de5d7f03b9ac7eb0ca49bb46d5edef060bd440
Gabriela Coy <42784440+GabiMath@users.noreply.github.com>
2021-11-30 10:50:10 -0500

    Add files via upload

--- Ejercicio11.js
+++ Ejercicio11.js
@@ -4,17 +4,43 @@ class Geolocalización {
     }
 
     mapa(){
-        const centro = {lat: 43.3548057, lng: -5.8534646};
-        const mapaGeoposicionado = new google.maps.Map(document.getElementById('mapa'),{
-            zoom: 11,
+        var centro = {lat: 43.3672702, lng: -5.8502461};
+        var mapaGeoposicionado = new google.maps.Map(document.getElementById('mapa'),{
+            zoom: 10,
             center:centro,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         });
-        const marker = new google.maps.Marker({
-            position: centro,
-            map: mapaGeoposicionado,
-          });
-    }
+
+        
+        var infoWindow = new google.maps.InfoWindow();
+        if (navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition(function(position) {
+                const pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                  };
+                infoWindow.setPosition(pos);
+                infoWindow.setContent("Location found.");
+                infoWindow.open(mapaGeoposicionado);
+                mapaGeoposicionado.setCenter(pos);
+                console.log(pos)
+                
+              }, function() {
+                handleLocationError(true, infoWindow, mapaGeoposicionado.getCenter());
+              });
+            } else {
+              // Browser doesn't support Geolocation
+              handleLocationError(false, infoWindow, mapaGeoposicionado.getCenter());
+            }
+          }
+    
+        handleLocationError(browserHasGeolocation, infoWindow, pos) {
+            infoWindow.setPosition(pos);
+            infoWindow.setContent(browserHasGeolocation ?
+                                  'Error: Ha fallado la geolocalización' :
+                                  'Error: Su navegador no soporta geolocalización');
+            infoWindow.open(mapaGeoposicionado);
+          }
 
 }
 var miPosicion = new Geolocalización();
