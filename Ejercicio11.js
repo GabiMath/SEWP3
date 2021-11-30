1ed6b5284a550f6217d1022efd89f41d92daa15c
Gabriela Coy <42784440+GabiMath@users.noreply.github.com>
2021-11-30 08:27:32 -0500

    Add files via upload

--- Ejercicio11.js
+++ Ejercicio11.js
@@ -1,9 +1,10 @@
 "use strict";
 class Geolocalización {
     constructor (){
-        navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this));
+        navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this), this.verErrores.bind(this));
     }
     getPosicion(posicion){
+        this.mensaje = "Se ha realizado correctamente la petición de geolocalización";
         this.longitud         = posicion.coords.longitude; 
         this.latitud          = posicion.coords.latitude;  
         this.precision        = posicion.coords.accuracy;
@@ -11,6 +12,22 @@ class Geolocalización {
         this.precisionAltitud = posicion.coords.altitudeAccuracy;
         this.rumbo            = posicion.coords.heading;
         this.velocidad        = posicion.coords.speed;       
+    }
+    verErrores(error){
+        switch(error.code) {
+        case error.PERMISSION_DENIED:
+            this.mensaje = "El usuario no permite la petición de geolocalización"
+            break;
+        case error.POSITION_UNAVAILABLE:
+            this.mensaje = "Información de geolocalización no disponible"
+            break;
+        case error.TIMEOUT:
+            this.mensaje = "La petición de geolocalización ha caducado"
+            break;
+        case error.UNKNOWN_ERROR:
+            this.mensaje = "Se ha producido un error desconocido"
+            break;
+        }
     }
     getLongitud(){
         return this.longitud;
@@ -23,10 +40,10 @@ class Geolocalización {
     }
     verTodo(dondeVerlo){
         var ubicacion=document.getElementById(dondeVerlo);
-        var datos=''; 
+        var datos='<p>'+ this.mensaje + '</p>'; 
         datos+='<p>Longitud: '+this.longitud +' grados</p>'; 
         datos+='<p>Latitud: '+this.latitud +' grados</p>';
-        datos+='<p>Precisión de la latitud y longitud: '+ this.precision +' metros</p>';
+        datos+='<p>Precisión de la longitud y latitud: '+ this.precision +' metros</p>';
         datos+='<p>Altitud: '+ this.altitude +' metros</p>';
         datos+='<p>Precisión de la altitud: '+ this.precisionAltitud +' metros</p>'; 
         datos+='<p>Rumbo: '+ this.rumbo +' grados</p>'; 
